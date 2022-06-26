91c34fe5c7851399c7b608d805412a1cafe5b021
Try3D <94473657+Try3D@users.noreply.github.com>
2022-06-26 12:36:18 +0530

    Remove mistake in previous commit

--- nvim/.config/nvim/lua/try/tokyonight.lua
+++ nvim/.config/nvim/lua/try/tokyonight.lua
@@ -4,23 +4,3 @@ vim.g.tokyonight_transparent = true
 
 -- Load the colorscheme
 vim.cmd[[colorscheme tokyonight]]
-
-    local hl = function(thing, opts)
-        vim.api.nvim_set_hl(0, thing, opts)
-    end
-
-    hl("CursorLineNR", {
-        bg = "None"
-    })
-
-    hl("Normal", {
-        bg = "none"
-    })
-
-    hl("LineNr", {
-        fg = "#5eacd3"
-    })
-
-    hl("netrwDir", {
-        fg = "#5eacd3"
-    })
