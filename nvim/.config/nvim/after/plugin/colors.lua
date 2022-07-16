f8d4d7fd96936c5ff55cfa2ed1acd32a389da894
Try3D <94473657+Try3D@users.noreply.github.com>
2022-07-16 21:52:07 +0530

    Stuff

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -3,4 +3,8 @@ vim.g.tokyonight_lualine_bold = true
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_sidebars = { "qf", "packer" }
 
+vim.g.tokyonight_colors = {
+    comment = "#414868"
+}
+
 vim.cmd [[colorscheme tokyonight]]
